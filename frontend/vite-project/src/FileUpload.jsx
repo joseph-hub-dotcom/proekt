617da0967b43d46dda3b44e3cfea6ca72d913f1a
Josif Al Shbul <joseph-hub-dotcom@users.noreply.github.com>
2024-10-04 22:17:04 +0200

    Merged my-old-code

--- frontend/vite-project/src/FileUpload.jsx
+++ frontend/vite-project/src/FileUpload.jsx
@@ -18,9 +18,7 @@ const FileUpload = (props) => {
     const fetchImages = async () => {
       setLoading(true); // Set loading to true
       try {
-        const response = await axios.get(
-          "https://proekt.onrender.com/api/photos"
-        );
+        const response = await axios.get("http://localhost:3000/api/photos");
         setImages(response.data);
       } catch (error) {
         console.error("Error fetching images:", error);
@@ -69,10 +67,9 @@ const FileUpload = (props) => {
           "Content-Type": "multipart/form-data",
         },
       });
+
       // Fetch images again to see the newly uploaded images
-      const response = await axios.get(
-        "https://proekt.onrender.com/api/photos"
-      );
+      const response = await axios.get("http://localhost:3000/api/photos");
       setImages(response.data);
       setUploadMessage(`Your uploads are live!`);
 
@@ -90,7 +87,7 @@ const FileUpload = (props) => {
 
   const handleDelete = async (id) => {
     try {
-      await axios.delete(`https://proekt.onrender.com/api/photos/delete/${id}`);
+      await axios.delete(`http://localhost:3000/api/photos/delete/${id}`);
       // Optionally, refetch images or update state
       setImages(images.filter((image) => image._id !== id)); // Update the local state to remove the deleted image
     } catch (error) {
@@ -101,7 +98,7 @@ const FileUpload = (props) => {
   // Function to delete all images
   const handleDeleteAll = async () => {
     try {
-      await axios.delete("https://proekt.onrender.com/api/photos");
+      await axios.delete("http://localhost:3000/api/photos");
       // Clear images after deletion
       setImages([]);
     } catch (error) {
@@ -113,7 +110,7 @@ const FileUpload = (props) => {
   const downloadAllImages = async () => {
     try {
       const response = await axios.get(
-        "https://proekt.onrender.com/api/photos/download",
+        "http://localhost:3000/api/photos/download",
         {
           responseType: "blob", // Important for handling binary data
         }
